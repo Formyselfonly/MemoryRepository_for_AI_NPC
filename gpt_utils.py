90de878e5e1b345333584e06062411e53592125a
For <z1597006376@gmail.com>
2023-11-29 17:11:41 +0800

    Initial commit

---
+++ gpt_utils.py
@@ -0,0 +1,110 @@
+from openai import OpenAI
+import os
+import dotenv
+from pathlib import Path
+dotenv.load_dotenv()
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+openai=OpenAI(
+    api_key=OPENAI_API_KEY
+)
+
+def openai_reply(prompt):
+    completions=openai.chat.completions.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[
+            {"role":"system","content":"You're my assistant"},
+            {"role":"user","content":prompt}
+        ]
+    )
+    return completions.choices[0].message.content
+
+def openai_reply_json(prompt):
+    completions=openai.chat.completions.create(
+      model="gpt-3.5-turbo-1106",
+      response_format={ "type": "json_object" },
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant designed to output JSON."},
+        {"role": "user", "content": prompt}
+      ]
+    )
+    return completions.choices[0].message.content
+
+
+def openai_reply_embedding(input):
+    completions=openai.embeddings.create(
+    input=input,
+    model="text-embedding-ada-002"
+    )
+    return completions.data[0].embedding
+
+def openai_fine_tuning(file_json):
+    openai.files.create(
+        # file=open("mydata.jsonl", "rb"),
+        file=open(file_json, "rb"),
+        purpose="fine-tune"
+    )
+    openai.fine_tuning.jobs.create(
+        training_file="trainingfile-abc123",
+        model="gpt-3.5-turbo-1106"
+    )
+
+def openai_fine_tuning_using(fine_tuning_model,prompt):
+    # model = "fine_tuning_modelft:gpt-3.5-turbo:my-org:custom_suffix:id",
+    completions = openai.chat.completions.create(
+        model = fine_tuning_model,
+        messages = [
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content":prompt}
+        ]
+    )
+    return completions.choices[0].message
+
+def openai_image_reply(prompt):
+    completions = openai.images.generate(
+        model="dall-e-3",
+        prompt=prompt,
+        size="1024x1024",
+        quality="standard",
+        n=1,
+    )
+    image_url = completions.data[0].url
+
+def openai_vision_reply(prompt):
+    completions = openai.chat.completions.create(
+        model="gpt-4-vision-preview",
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {"type": "text", "text": "What’s in this image?"},
+                    {
+                        "type": "image_url",
+                        "image_url": {
+                            "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+                        },
+                    },
+                ],
+            }
+        ],
+        max_tokens=300,
+    )
+    return completions.choices[0]
+
+def openai_text2speech_reply(input,speech_file_obj_path):
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = openai.audio.speech.create(
+        model="tts-1",
+        voice="alloy",
+        input=input
+    )
+    response.stream_to_file(speech_file_obj_path)
+
+
+def openai_speech2text_reply(audio_input):
+    # audio_file = open("/path/to/file/audio.mp3", "rb")
+    audio_file = open(audio_input, "rb")
+    transcript = openai.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file
+    )
+    return transcript
---
+++ main.py
@@ -0,0 +1,42 @@
+import dotenv
+from gpt_utils import openai_reply
+from memory_repository import summary
+
+
+# 进度
+# Short-Term Memory实现了
+# Summary 实现了
+# 现在需要实现forget和Long-Term Memory
+def memoryrepository_talk(summary_and_forgetting_frequence):
+    res = ""
+    round = 1
+    short_term_memory = [""]
+    short_term_memory_str = ""
+    long_term_memory_str = ""
+    metaprompt = "You're my assistant to help me!"
+    while True:
+        user_input=input("-----Round{}-----\n".format(round))
+        print("------user_input------",user_input)
+        # short_term_memory=user_input+res
+
+        # print("------The type of short_term_memory_str------",type(short_term_memory_str))
+        print("------short_term_memory_str------",short_term_memory_str)
+        prompt=metaprompt+long_term_memory_str+short_term_memory_str+user_input
+        print("------prompt------", prompt)
+
+        res=openai_reply(prompt)
+        short_term_memory.append(user_input+res)
+        # print("------short_term_memory------", short_term_memory)
+        short_term_memory_str=str(short_term_memory)
+
+        if round%summary_and_forgetting_frequence==0:
+            long_term_memory_str = summary(short_term_memory_str)
+            short_term_memory_str=""
+
+        print("------long_term_memory_str------",long_term_memory_str)
+        print("------res------",res)
+        round=round+1
+
+# In test environment,we set it to 2
+# In development environment,we set it to 5
+memoryrepository_talk(2)
